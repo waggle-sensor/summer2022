3f31207f6be850ee563516cef28a252b026908a9
IsaiahPritchard <106760508+IsaiahPritchard@users.noreply.github.com>
2022-07-19 00:13:19 +0530

    Update secondarys.py

--- pritchard/Plugin-EdgeSwitchMetrics/secondarys.py
+++ pritchard/Plugin-EdgeSwitchMetrics/secondarys.py
@@ -2,6 +2,22 @@ import queue
 import matplotlib.pyplot as plt
 import myconfig
 import statisticgetter
+"""
+def noIndexGetter(jsonObject, variable):
+        if(variable=='timestamp'):
+        elif(variable=='uptime'):
+        elif(variable=='processor usage'):
+        elif(variable=='usage' or variable=='free' or variable=='total'):(ram)
+        return(variable, value)
+def withIndexGetter(jsonObject, index, variable):
+        if(variable=='name' or variable=='value' or variable=='type'):#this triggers if temperature info is requested, max index of 3
+        else:
+              #this triggers if portsinformation is requested, port= 0-15
+              #variable= dropped errors txErrors rxErrors rate txRate rxRate bytes txBytes rxBytes packets txPackets rxPackets pps txPPS rxPPS poePower  
+        return(variable, indexstring, value)
+
+
+"""
 def Getshortenedlist(firsttimestamp, secondtimestamp):
     shortlist =[]
     for i in range(len(myconfig.Listofjson)):
@@ -11,27 +27,7 @@ def Getshortenedlist(firsttimestamp, secondtimestamp):
         if(timestamp>secondtimestamp):
             break
     return(shortlist)
-def getStatisticPlot(variable, port, firsttimestamp, secondtimestamp):
-    shortlist=Getshortenedlist(firsttimestamp, secondtimestamp)
-    Timestamps=[0]*len(shortlist)
-    Poes=[]
-    values=[0]*len(shortlist)
-    for x in range(len(shortlist)):
-        Timestamps[x], json = shortlist[x]
-        values[x], othertimestamps=statisticgetter.getStatistics(json,port,variable)
-    #print(Timestamps)
-    #print(values)
-    plt.plot(Timestamps, values)
-    plt.xlabel('Timestamps')
-    plt.ylabel('variable')
-    plt.title('variable Vs Time')
-    plt.show()
-    
-def getPlotofbufferStatistic(variable, port):
-    Listoftimestamps=getlisttimestamps()
-    firsttimestamp=Listoftimestamps[0]
-    lasttimestamp=Listoftimestamps[-1]
-    getStatisticPlot(variable, port, firsttimestamp, lasttimestamp)
+
 
 def getlisttimestamps():
         Timestamps= [0 for i in range(len(myconfig.Listofjson))]
@@ -47,44 +43,93 @@ def getJson(Timestamp):
                         break
         return json
 
-
-def getPOEAverages():
+"""
+def noIndexGetter(jsonObject, variable):
+        if(variable=='timestamp'):
+        elif(variable=='uptime'):
+        elif(variable=='processor usage'):
+        elif(variable=='usage' or variable=='free' or variable=='total'):(ram)
+        return(variable, value)
+def withIndexGetter(jsonObject, index, variable):
+        if(variable=='name' or variable=='value' or variable=='type'):#this triggers if temperature info is requested, max index of 3
+        else:
+              #this triggers if portsinformation is requested, port= 0-15
+              #variable= dropped errors txErrors rxErrors rate txRate rxRate bytes txBytes rxBytes packets txPackets rxPackets pps txPPS rxPPS poePower  
+        return(variable, indexstring, value)
+"""
+def getAverages(variable):
         #Averages = [0] * numberofports
         #Averages= [0.0 for i in range(numberofports)]
-        Averages =[(0,0)*myconfig.numberofports]# error creates (0, 0, 0, 0, 0, 0)
+        Averages =[(0,0)]*myconfig.numberofports# error creates (0, 0, 0, 0, 0, 0)
         #print (type(Averages[0]))
         for y in range(len(myconfig.Listofjson)):
                 time, jsonObject=myconfig.Listofjson[y]
               
                 for x in range(myconfig.numberofports): #iterates through each port, 8-15 in final
-                        poePower, time = statisticgetter.getStatistics(jsonObject,1,'poePower')
-                        temp =Averages[x][0]+(poePower-Averages[x][0])/(y+1) #rolling average calculation
-                        Averages[x]=(temp, time)
-        return (Averages)                       
-def getPOEMaxes():
-        Maximums =[(0,0)*myconfig.numberofports]
+                        variable, indexstring, value= statisticgetter.withIndexGetter(jsonObject,x,variable)
+                        temp =Averages[x][0]+(value-Averages[x][0])/(y+1) #rolling average calculation
+                        Averages[x]=(temp, time)#this needs to be fixed to give the closest time to when the average occured
+        return (Averages, variable)
+def isnormalcheck(variable, standardvalue, currentvalue):#later add in details on acceptable ratio
+        if standardvalue== 0 and currentvalue==0:
+                proportion=1
+        elif standardvalue== 0:
+                proportion= 3*currentvalue
+        else: proportion=currentvalue/standardvalue
+        if(proportion>1.5):
+                return(variable,"raised is now this fraction of former value:",proportion)
+        elif(proportion<0.75):
+                return(variable,"dropped is now this fraction of former value:",proportion)
+        else: return None
+
+
+
+def getMaxes(variable):
+        Maximums =[(0,0)]*myconfig.numberofports
         for y in range(len(myconfig.Listofjson)):#I previously passed in a copy of the Que of Jsons
                 print(y)
-                jsonObject=myconfig.Listofjson[y]
+                time, jsonObject=myconfig.Listofjson[y]
                 print('max should end')
                 for x in range(myconfig.numberofports): #iterates through each port, 8-15 in final, I previously stored the number of ports locally
-                        poePower, time= statisticgetter.getStatistics(jsonObject,1,'poePower')
-                        if poePower>Maximums[x][0]:
-                                Maximums[x]=(poePower,time)
-        
-        return (Maximums)
-def getPOEMins():  
+                        variable, indexstring, value= statisticgetter.withIndexGetter(jsonObject,x,variable)
+                        if value>Maximums[x][0]:
+                                Maximums[x]=(value,time)
+        return (Maximums, variable)
+def getMins(variable):  
         #timestamps = [0] * 8#not sure how to initialize list of timestamps
-        Minimums =[(150,0)*myconfig.numberofports]
+        Minimums =[(150,0)]*myconfig.numberofports
         #Minimums = [0] * 8
         #Minimums = [150 for i in range(8)]#assumes that no port will have a higher minimum then 150 watts
         for y in range(len(myconfig.Listofjson)):
-                jsonObject=myconfig.Listofjson[y]
+                time, jsonObject=myconfig.Listofjson[y]
                 for x in range(myconfig.numberofports): #iterates through each port, 8-15 in final
-                        poePower, time= statisticgetter.getStatistics(jsonObject,1,'poePower')
-                        if poePower<Minimums[x][0]: #compars and sets maximums and mins per port
-                                Minimums[x]=(poePower,time)                    
-        return (Minimums)
+                        variable, indexstring, value= statisticgetter.withIndexGetter(jsonObject,x,variable)
+                        if value<Minimums[x][0]: #compars and sets maximums and mins per port
+                                Minimums[x]=(value,time)                    
+        return (Minimums, variable)
+
+"""
+    def getStatisticPlot(variable, index, firsttimestamp, secondtimestamp):
+    shortlist=Getshortenedlist(firsttimestamp, secondtimestamp)
+    Timestamps=[0]*len(shortlist)
+    values=[0]*len(shortlist)
+    for x in range(len(shortlist)):
+        Timestamps[x], json = shortlist[x]
+        values[x], othertimestamps=statisticgetter.withIndexGetter(json,index,variable)
+    #print(Timestamps)
+    #print(values)
+    plt.plot(Timestamps, values)
+    plt.xlabel('Timestamps')
+    plt.ylabel('variable')
+    plt.title('variable Vs Time')
+    plt.show()
+    
+def getPlotofbufferStatistic(variable, index):
+    Listoftimestamps=getlisttimestamps()
+    firsttimestamp=Listoftimestamps[0]
+    lasttimestamp=Listoftimestamps[-1]
+    getStatisticPlot(variable, index, firsttimestamp, lasttimestamp)
+    """
 '''''
 def getonoff(Listofjson): #there is o logic flaw in here that only takes note of the first time the value of isonrecent is changed
         onoff =[(True,time.time())]
@@ -92,8 +137,8 @@ def getonoff(Listofjson): #there is o logic flaw in here that only takes note of
         isonrecent =  [False for i in range(numberofports)]
         for y in range(buffer_len(myconfig.Listofjson)):
                 for x in range(numberofports): #iterates through each port, 8-15 in final
-                        time, poePower = getStatistics(jsonObject,1,'poePower')
-                        if poePower!=0:#checks to see if anything currently plugged in
+                        time, value = getStatistics(jsonObject,1,'value')
+                        if value!=0:#checks to see if anything currently plugged in
                                 isoncurrent[x]= True
                         else:
                                 isoncurrent[x]= False
@@ -102,4 +147,4 @@ def getonoff(Listofjson): #there is o logic flaw in here that only takes note of
                                 wattagerecorders[x].put(isoncurrent[x])
                                 print(y)
                         isonrecent[x]=isoncurrent[x]   
-  '''
+  '''        
