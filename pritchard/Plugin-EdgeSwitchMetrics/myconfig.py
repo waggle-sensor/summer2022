4e1c43443188464a542abe95c87aa7e566cb6fbd
IsaiahPritchard <106760508+IsaiahPritchard@users.noreply.github.com>
2022-07-23 02:59:37 +0530

    Update myconfig.py

--- pritchard/Plugin-EdgeSwitchMetrics/myconfig.py
+++ pritchard/Plugin-EdgeSwitchMetrics/myconfig.py
@@ -17,16 +17,16 @@ numberofareastemp= 4
         else:dropped errors txErrors rxErrors rate txRate rxRate bytes txBytes rxBytes packets txPackets rxPackets pps txPPS rxPPS poePower, max index 15  
         return(variable, indexstring, value)
 """
-variableboundaries = [ {'value': (0,0), 'poePower':(1,50),'rxRate':(0,100)},#port 1 (lowerboundary,upperboundary)
-{'value': (50,80), 'poePower':(1,50),'rxRate':(0,100)},#port 2 or area2 depending on whether checking temp or ports
-{'value': (100,500), 'poePower':(0,50),'rxRate':(0,7000)},
-{'value': (0,0), 'poePower':(0,50),'rxRate':(0,7000)},
-{'poePower':(0,50),'rxRate':(0,100)},#there are only 4 areas temperatures that can be read
-{'poePower':(0,50),'rxRate':(0,100)},
-{'poePower':(0,50),'rxRate':(0,100)},
-{'poePower':(0,50),'rxRate':(0,5000)}
+variableboundaries = [ {'value': (0,100), 'poePower':(0,0),'rate':(0,100)},#port 1 (lowerboundary,upperboundary)
+{'value': (50,80), 'poePower':(0,50),'rate':(0,100)},#port 2 or area2 depending on whether checking temp or ports
+{'value': (0,500), 'poePower':(0,50),'rate':(0,7000)},
+{'value': (0,500), 'poePower':(0,50),'rate':(0,7000)},
+{'poePower':(0,50),'rate':(0,7000)},#there are only 4 areas temperatures that can be read
+{'poePower':(0,50),'rate':(0,7000)},
+{'poePower':(0,50),'rate':(0,100)},
+{'poePower':(0,50),'rate':(0,900000)}
 ]
-rxOnBoundary=0 #this is the boundary on which a port is concidered on based on received packages
+rateOnBoundary=0 #this is the boundary on which a port is concidered on based on received packages
 
 
 #for unused proportion function
